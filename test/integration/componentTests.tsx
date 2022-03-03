c2328fd18b766850b870424ac8059da680d354e7
Noah Hummel <noah.hummel@thenativeweb.io>
2022-03-03 10:12:15 +0100

    fix: fix re-rendering on theme switch

--- test/integration/componentTests.tsx
+++ test/integration/componentTests.tsx
@@ -1,7 +1,7 @@
 import { assert } from 'assertthat';
-import React from 'react';
+import React, { ComponentProps, FunctionComponent, ReactElement, useContext } from 'react';
 import styled from 'styled-components';
-import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { act, cleanup, render, screen, waitFor } from '@testing-library/react';
 import { createGlobalThemeProvider, createLocalTheme } from '../../lib';
 import { Length, px } from '@nhummel/css-in-js';
 
@@ -14,7 +14,7 @@ const configuration = {
       background: '#222'
     },
     light: {
-      space: (units: number): Length => Length.new(units * 4, px),
+      space: (units: number): Length => Length.new(units * 8, px),
       brandColor: '#a5e',
       color: '#222',
       background: '#e3e3e3'
@@ -92,7 +92,80 @@ suite('Component tests', (): void => {
   });
   suite('useTheme hook', (): void => {
     test('can be used to switch the variant, re-rendering the component.', async (): Promise<void> => {
+      const { globalTheme, GlobalThemeProvider, useTheme } = createGlobalThemeProvider(configuration);
+      const { get } = createLocalTheme({
+        globalTheme,
+        themeFactory ({ theme, variant }) {
+          const { brandColor, background } = theme;
+          let { color } = theme;
 
+          if (variant === 'light') {
+            color = background;
+          }
+
+          return {
+            color,
+            background: brandColor,
+            padding: theme.space(2)
+          };
+        }
+      });
+
+      const Banner = styled.div`
+        color: ${get(theme => theme.color)};
+        background-color: ${get(theme => theme.background)};
+        padding: ${get(theme => theme.padding)};
+      `;
+
+      const ThemeSwitch: FunctionComponent = () => {
+        const { switchVariant } = useTheme();
+
+        return (
+          <button
+            data-testid='switchTheme'
+            onClick={ (): void => switchVariant('light') }
+          >
+            Switch theme
+          </button>
+        );
+      };
+
+      render((
+        <GlobalThemeProvider>
+          <Banner data-testid='banner'>Yes yes</Banner>
+          <ThemeSwitch />
+        </GlobalThemeProvider>
+      ));
+
+      await waitFor(async () => {
+        const banner = await screen.findByTestId('banner');
+        const bannerStyle = window.getComputedStyle(banner);
+
+        assert.that(bannerStyle.color).is.equalTo('rgb(227, 227, 227)');
+        assert.that(bannerStyle.background).is.equalTo('rgb(170, 85, 238)');
+        assert.that(bannerStyle.padding).is.equalTo('8px');
+      });
+
+      await waitFor(async () => {
+        const switchButton = await screen.findByTestId('switchTheme');
+
+        act(() => {
+          switchButton.click();
+        });
+      });
+
+      await new Promise((resolve): void => {
+        setTimeout(resolve, 300);
+      });
+
+      await waitFor(async () => {
+        const banner = await screen.findByTestId('banner');
+        const bannerStyle = window.getComputedStyle(banner);
+
+        assert.that(bannerStyle.color).is.equalTo('rgb(227, 227, 227)');
+        assert.that(bannerStyle.background).is.equalTo('rgb(170, 85, 238)');
+        assert.that(bannerStyle.padding).is.equalTo('16px');
+      });
     });
     test('can be used to obtain the available variants.', async (): Promise<void> => {
 
